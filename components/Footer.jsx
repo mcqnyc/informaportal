13c99cb32713e34653374010568d073a7b0f687b
mcqnyc <mcqnyc@gmail.com>
2017-01-19 08:37:59 +0000

    feat($client): create header and footer components
    create header and footer components
    
    none

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,1 +1,41 @@
-Footer.jsx
+const React = require('react');
+const { Link } = require('react-router');
+const { connect } = require('react-redux');
+
+const Footer = React.createClass ({
+  render () {
+    return (
+      <div class="container">
+        <section class="footer-nav">
+          <nav class="channels">
+            <h3>Channels</h3>
+            <ul>
+              <li><a href="#">Art&nbsp;&amp;&nbsp;Design</a></li>
+              <li><a href="#">Automobiles</a></li>
+              <li><a href="#">Books</a></li>
+              <li><a href="#">Business</a></li>
+              <li><a href="#">Celebrity</a></li>
+              <li><a href="#">Education</a></li>
+              <li><a href="#">Fashion</a></li>
+              <li><a href="#">Food</a></li>
+              <li><a href="#">Health</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">Music</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Politics</a></li>
+              <li><a href="#">Real&nbsp;Estate</a></li>
+              <li><a href="#">Science</a></li>
+              <li><a href="#">Sports</a></li>
+              <li><a href="#">Technology</a></li>
+              <li><a href="#">Travel</a></li>
+              <li><a href="#">TV</a></li>
+            </ul>
+          </nav>
+        </section>
+      </div>
+    )
+  }
+});
+
+module.exports = connector(Footer);
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,44 +1,33 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
-import { clearError, facebookAuthReq } from '../actions/actionCreators'
+const React = require('react');
+const { Link } = require('react-router');
+const { connect } = require('react-redux');
 
-class Header extends Component {
-
-  renderLinks() {
-    if (this.props.authenticated) {
-      return (
-        <div className="headerBtns">
-          <Link className="headerBtn" to="/signout">Sign Out</Link>
-        </div>
-      )
-    } else {
-      return (
-        <div className="headerBtns">
-          <div className="headerBtn"><Link to="/signin">Sign In</Link></div>
-          <div className="headerBtn"><Link to="/signup" onClick={this.props.clearError}>Sign Up</Link></div>
-          <div className="headerBtn"><a href="/api/login/facebook">Log In with Facebook</a></div>
-        </div>
-      );
-    }
-  }
-
-  render() {
+const Header = React.createClass ({
+  render () {
     return (
-      <div className="headerBar">
-        <div className="headerTitle"><Link to="/">OPEN MUSEUM</Link></div>
-        {this.renderLinks()}
-      </div>
-    );
+      <nav class="navbar navbar-inverse">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <Link to="/" class="navbar-brand">Omnichanne1</Link>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li class="active"><Link to="/login"><span class="glyphicon glyphicon-log-in"></span> Login</Link></li>
+              <li><Link to="/signup">Signup</Link></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="mailto:mcqnyc@gmail.com">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
   }
+});
 
-}
-
-function mapStateToProps(state) {
-  return {
-    authenticated: state.auth.authenticated
-  };
-}
-
-export default connect(mapStateToProps, { clearError, facebookAuthReq })(Header);
-
+module.exports = connector(Header);
