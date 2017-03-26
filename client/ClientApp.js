f913deaa0e6e3200dea90adfbdf413db03050c2c
mcqnyc <mcqnyc@gmail.com>
2017-03-26 19:36:33 -0400

    feat($footer): create footer component
    create basic footer component - needs interactivity
    
    none

--- client/ClientApp.js
+++ client/ClientApp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
 // import '../public/css/reset.css';
 // import '../public/css/style.css';
 
@@ -8,12 +9,13 @@ const App = React.createClass({
   render () {
     return (
       <div className='app'>
+      <Header />
         <div className='landing'>
-          <Header />
           <h1>Omnichanne1</h1>
           <input type='text' placeholder='Search' />
           <a>or Browse All</a>
         </div>
+      <Footer />
       </div>
     );
   }
