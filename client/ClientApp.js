292b8005fb3fda57a8f22f20cd5b1d39fe5a2364
mcqnyc <mcqnyc@gmail.com>
2017-03-26 19:02:06 -0400

    feat($header): create basic header - no interactivity
    created basic header; need to make it interactive and style it
    
    none

--- client/ClientApp.js
+++ client/ClientApp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Header from '../components/Header.jsx';
 // import '../public/css/reset.css';
 // import '../public/css/style.css';
 
@@ -8,6 +9,7 @@ const App = React.createClass({
     return (
       <div className='app'>
         <div className='landing'>
+          <Header />
           <h1>Omnichanne1</h1>
           <input type='text' placeholder='Search' />
           <a>or Browse All</a>
