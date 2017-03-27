76b6ee22b8e3cae869651fec221c10ee494c803f
mcqnyc <mcqnyc@gmail.com>
2017-03-26 20:13:59 -0400

    feat($dataViewport): create basic DataViewport component
    create basic DataViewport component
    
    none

--- client/ClientApp.js
+++ client/ClientApp.js
@@ -2,20 +2,34 @@ import React from 'react';
 import { render } from 'react-dom';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
+import DataViewport from '../components/DataViewport.jsx';
 // import '../public/css/reset.css';
 // import '../public/css/style.css';
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>
-      <Header />
-        <div className='landing'>
-          <h1>Omnichanne1</h1>
-          <input type='text' placeholder='Search' />
-          <a>or Browse All</a>
+      <div className="app">
+
+        <Header />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <DataViewport />
+            </div>
+            <div className="col-sm-4">
+              <DataViewport />
+            </div>
+            <div className="col-sm-4">
+              <DataViewport />
+            </div>
+          </div>
         </div>
-      <Footer />
+
+
+        <Footer />
+
       </div>
     );
   }
