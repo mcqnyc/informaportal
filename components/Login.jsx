9e31d39940fa78de7bc7823d62d5fd10f132e5aa
mcqnyc <mcqnyc@gmail.com>
2017-03-26 21:36:33 -0400

    feat($app): create basic Login and Signup components
    create basic Login and Signup components - need to add interactivity
    
    none

---
+++ components/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class Login extends React.Component {
+  render () {
+    return (
+      <section className="auth">
+        <div className="row">
+          <div className="col-sm-12 col-sm-offset-3">
+            <h2>Login</h2>
+            <form name="loginForm">
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <button>Login</button>
+            </form>
+            <Link to="signup">Don't have an account? <strong>Signup</strong> ...</Link>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Login;
