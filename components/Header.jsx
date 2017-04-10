4bd33d875e01c991a873cba6e28036173547ced0
mcqnyc <mcqnyc@gmail.com>
2017-04-10 18:00:40 -0400

    feat($UI: header): create hamburger toggle menu
    basic implementation of hamburger toggle menu that is fully responsive but needs to be styled
    correctly
    
    none

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,31 +4,22 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
   render () {
     return (
-      <nav className="navbar navbar-inverse">
-        <div className="container">
-          
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" id="hamburger">
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <Link to="/" className="navbar-brand">Omnichanne1</Link>
-          </div>
+    
 
-          
-          <div className="navbar-collapse collapse" id="myNavbar">
-            <ul className="nav navbar-nav">
-              <li className="active"><Link to="/login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
-              <li><Link to="/signup">Signup</Link></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li><a href="mailto:mcqnyc@gmail.com">Contact</a></li>
-            </ul>
-          </div>
+        <nav className="navbar">
+          <Link to="/" className="logo">Omnichanne1</Link>
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+          <ul className="menu">
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </ul>
+          <ul className="menu-right">
+            <li><a href="mailto:mcqnyc@gmail.com">Contact</a></li>
+          </ul>
+        </nav>
 
-        </div>
-      </nav>
+        
     );
   }
 }
