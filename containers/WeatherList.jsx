43db9c193716a98292d18100c892c033540393bf
mcqnyc <mcqnyc@gmail.com>
2017-06-15 12:39:48 -0400

    feat($weather API): implement Sparklines charts and Google Maps
    implement Sparklines charts and Google Maps
    
    none

--- containers/WeatherList.jsx
+++ containers/WeatherList.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/Chart.jsx'
+import GoogleMap from '../components/GoogleMap.jsx'
 
 class WeatherList extends React.Component {
   renderWeather(cityData){
     const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="&deg;C" /></td>
+        <td><Chart data={humidities} color="green" units="%" /></td>
       </tr>
     );
   }
@@ -18,8 +25,8 @@ class WeatherList extends React.Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Humidity</th>
+            <th>Temperature (&deg;C)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
