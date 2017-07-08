3ecc00330c1f817d01244eb9eb972343596264e8
mcqnyc <mcqnyc@gmail.com>
2017-07-08 17:01:15 -0400

    fix($API: weather): handle bug when misspelled city name is entered
    emit an alert when anything other than a 200 success code is returned from the weather API
    
    none

--- reducers/reducer_weather.js
+++ reducers/reducer_weather.js
@@ -4,6 +4,10 @@ export default function (state = [], action) {
   console.log('action recvd', action);
   switch (action.type) {
     case FETCH_WEATHER:
+      if (action.payload.status !== 200) {
+        alert('Please check the spelling of the city name that you entered.');
+        // .catch(console.log("no DATA at GeoFromSearch"))
+      }
       return state.concat(action.payload.data);
       // return [ action.payload.data, ...state, console.log('FETCH_WEATHER state : ',state)];
     default:
