da40fd7ca864787e2988b939660ac4b0a427b3eb
mcqnyc <mcqnyc@gmail.com>
2017-07-15 17:12:57 -0400

    refactor($API: YouTube): create YouTube search form field
    use React-Boostrap to create form field and button group
    
    none

--- components/YouTube.jsx
+++ components/YouTube.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Loader from './Loader.jsx';
+import { Form, FormGroup, FormControl, Button, InputGroup, ResponsiveEmbed } from 'react-bootstrap';
 
 class YouTube extends React.Component {
   constructor(props) {
     super(props);
 
     this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.searchYouTube = this.searchYouTube.bind(this);
 
     this.state = {
       data: [],
-      loading: true,
-      searchTerm: 'new york fashion week',
+      isLoading: false,
+      searchTerm: 'Rei Kawakubo',
     };
   }
 
@@ -21,76 +22,94 @@ class YouTube extends React.Component {
     this.setState({searchTerm: event.target.value});
   }
 
+  componentDidMount() {
+    this.searchYouTube();
+  }
 
-  handleSubmit(event){
+  onFormSubmit(event) {
     event.preventDefault();
     this.searchYouTube();
   }
 
 
   searchYouTube() {
-
-    const myInit = { 
+    const myInit = {
       method: 'GET',
       mode: 'cors',
       cache: 'default',
     };
 
-    let { data, searchTerm } = this.state;
+    const { data, searchTerm } = this.state;
 
-    let request = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchTerm}&maxResults=3&order=viewCount&publishedAfter=2017-01-01T00:00:00Z&key=AIzaSyBAv3_8ITzuxD10Wc3HVeziuqmSW5EcP7A`;
+    const request = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${searchTerm}&maxResults=3&order=viewCount&publishedAfter=2017-01-01T00:00:00Z&key=AIzaSyBAv3_8ITzuxD10Wc3HVeziuqmSW5EcP7A`;
 
     fetch(request, { myInit })
       .then((response) => {
-        if(response.ok){
-          return response.json()
+        if (response.ok) {
+          return response.json();
         }
         throw new Error('Network response was not ok.');
       })
-      .then(json => {
-        this.setState({data: json.items, loading: false});
+      .then((json) => {
+        this.setState({ data: json.items, isLoading: false });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('this is a fetch error', error);
-    });
+      });
   }
 
 
-  componentDidMount () {
-    this.searchYouTube();
-  }
-
-
-  render () {
-    if(this.state.loading){
-      return (
-        <section className="youtube-api">
-          <Loader />
-        </section>
-      )
-    }
+  render() {
+    const isLoading = this.state.isLoading;
+    // if (this.state.isLoading) {
+    //   return (
+    //     <section className="youtube-api">
+    //       <Loader />
+    //     </section>
+    //   );
+    // }
 
     return (
       <section className="youtube-api">
         <h2>Watch YouTube</h2>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" placeholder="Search for something..." className="form-control" value={this.state.searchTerm} onChange={this.handleSearchTermChange} />
-            <input type="submit" className="btn btn-primary form-control" value="Search" />
-          </form>
+        <Form inline onSubmit={this.onFormSubmit} >
+          <FormGroup controlId="formInlineSearch">
+            <InputGroup>
+              <FormControl
+                type="text"
+                bsSize="lg"
+                placeholder="Search for something..."
+                value={this.state.searchTerm}
+                onChange={this.handleSearchTermChange}
+              />
+              <InputGroup.Button>
+                <Button
+                  type="submit"
+                  bsStyle="primary"
+                  disabled={isLoading}
+                  onClick={!isLoading ? null : null}
+                >
+                  {isLoading ? 'Loading...' : 'Search'}
+                </Button>
+              </InputGroup.Button>
+            </InputGroup>
+          </FormGroup>
+        </Form>
+
         <div id="youtube-results"></div>
           {this.state.data.map((item) => {
             return (
-              <div className="item" key={item.id.videoId} >
-                  <h5>{item.snippet.title}</h5>
-                  <iframe className="video" src={`https://www.youtube.com/embed/${item.id.videoId}`} frameBorder="0" allowFullScreen></iframe>
+              <div className="video-item" key={item.id.videoId} >
+                <h5>{item.snippet.title}</h5>
+                {/*<ResponsiveEmbed a16by9>*/}
+                <iframe className="video" src={`https://www.youtube.com/embed/${item.id.videoId}`} frameBorder="0" allowFullScreen></iframe>
+                {/*</ResponsiveEmbed>*/}
               </div>
             );
           })}
       </section>
     );
   }
-
-
 }
 
 export default YouTube;
