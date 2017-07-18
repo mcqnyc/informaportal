43494a825387763ba3a8be69df00b8b1bf8a4d08
mcqnyc <mcqnyc@gmail.com>
2017-07-18 18:20:51 -0400

    refactor($API: image search): improve image display/UI & initiate search on page load
    images display alongside each other >880px; search starts when page loads and images render
    afterwards
    
    none

--- containers/SearchImages.jsx
+++ containers/SearchImages.jsx
@@ -21,7 +21,7 @@ class SearchImages extends React.Component {
   }
 
   componentDidMount() {
-    // this.fetchImages(this.state.searchTerm);
+    this.fetchImages(this.state.searchTerm);
     this.setState({ searchTerm: '' });
   }
 
@@ -40,12 +40,6 @@ class SearchImages extends React.Component {
       imageQuery = 'fashion';
     }
 
-
-    // const { data, searchTerm } = this.state;
-    // const url = `${ROOT_URL}&q=${city}&days=5`;
-    // const request = axios.get(url);
-    // var myRequest = new Request('flowers.jpg');
-
     const API_KEY_IMAGES = '5bm2bb5nwpzgxk7puhm4w9vq';
     const ROOT_URL_IMAGES = `https://api.gettyimages.com/v3/search/images`;
     const myInit = {
@@ -54,17 +48,10 @@ class SearchImages extends React.Component {
       mode: 'cors',
       cache: 'default',
     };
-    const request = new Request(`${ROOT_URL_IMAGES}?phrase=${imageQuery}`, myInit);
-    
-    // let fetchImagesHeaders = {'Api-Key': API_KEY_IMAGES};
-    // console.log(fetchImagesHeaders, '...')
-    // let myHeaders = new Headers();
-    // myHeaders.append('Api-Key', API_KEY_IMAGES);
-    // console.log(myHeaders, 'myHeaders')
-    
 
+    const request = new Request(`${ROOT_URL_IMAGES}?phrase=${imageQuery}`, myInit);
     console.log(request);
-    // fetch(request, { myInit })
+
     fetch(request)
       .then((response) => {
         console.log(response);
@@ -81,40 +68,7 @@ class SearchImages extends React.Component {
       });
   }
 
-        // <pre><code>{JSON.stringify(this.state.data[0], null, 4)}</code></pre>
-
-// {
-//             // "id": "98844671",
-//             // "asset_family": "creative",
-//             // "caption": null,
-//             // "collection_code": "DV",
-//             // "collection_id": 13,
-//             // "collection_name": "DigitalVision",
-//             "display_sizes": [
-//                 {
-//                     "is_watermarked": false,
-//                     "name": "thumb",
-//                     "uri": "https://media.gettyimages.com/photos/side-view-of-woman-with-metallic-make-up-picture-id98844671?b=1&k=6&m=98844671&s=170x170&h=F5H4A2ag0zQPpMDKEut6c5bGdjX805ScTeZwVyqFoB0="
-//                 }
-//             ],
-//             // "license_model": "royaltyfree",
-//             // "max_dimensions": {
-//             //     "height": 4278,
-//             //     "width": 5700
-//             // },
-//             "title": "Side view of woman with metallic make up"
-//         }
-
-
   render() {
-    // if (this.state.isLoading) {
-    //   return (
-    //     <section className="youtube-api">
-    //       <Loader />
-    //     </section>
-    //   );
-    // }
-
     return (
       <section className="youtube-api">
         <Form inline onSubmit={this.onFormSubmit} >
@@ -142,10 +96,8 @@ class SearchImages extends React.Component {
 
         <div id="image-results" />
         {this.state.data.map(item => (
-          <div className="image-item" key={item.id} >
           
-            <img src={item.display_sizes[0].uri} className="img-responsive center-block"  alt={item.title} />
-          </div>
+            <img src={item.display_sizes[0].uri} key={item.id} className="img-responsive img-thumbnail"  alt={item.title} />
           ),
         )}
         
@@ -155,17 +107,3 @@ class SearchImages extends React.Component {
 }
 
 export default SearchImages;
-
-        // <Image
-          // style={{width: 50, height: 50}}
-          // source={{uri: item.display_sizes.uri}}
-        // />
- //       <Image
- //          style={{width: 50, height: 50}}
- //          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
- //        />
-
- // {`${item.display_sizes.uri}`} = undefined
- // {`{item.display_sizes.uri}`} = {item.display_sizes.uri}
- // {item.display_sizes.uri} = no image attr tag
-
