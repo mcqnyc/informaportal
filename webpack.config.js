b397acfa1bf65bfdfe4236fbd0655fe2e77bdb36
mcqnyc <mcqnyc@gmail.com>
2017-08-11 22:43:37 -0400

    build($webpack): add config for checking for and replacing NODE_ENV
    add new webpack.DefinePlugin configuration
    
    none

--- webpack.config.js
+++ webpack.config.js
@@ -56,11 +56,11 @@ module.exports = env => {
       ],
     },
     plugins: [
-      // new webpack.DefinePlugin({
-      //   'process.env': {
-      //     NODE_ENV: JSON.stringify('production'),
-      //   },
-      // }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
       new BabiliPlugin()
     ],
   }
