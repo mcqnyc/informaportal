fc11112243e51266c83b42b31299334355186f08
mcqnyc <mcqnyc@gmail.com>
2018-10-06 18:22:03 -0400

    refactor: css before writing first sass rule

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,23 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar">
-        <Link to="/" className="logo">Informaportal</Link>
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-        <ul className="menu">
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
-        </ul>
-        <ul className="menu-right">
-          <li><a href="mailto:mcqnyc@gmail.com">Contact</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
+const Navbar = () => (
+  <nav className="navbar">
+    <Link to="/" className="logo">Informaportal</Link>
+    <input className="menu-btn" type="checkbox" id="menu-btn" />
+    <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+    <ul className="menu">
+      <li><Link to="/login">Login</Link></li>
+      <li><Link to="/signup">Signup</Link></li>
+    </ul>
+    <ul className="menu-right">
+      <li><a href="mailto:mcqnyc@gmail.com">Contact</a></li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
